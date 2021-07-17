cbae98864f0d3fe8d4b81a2b2e0d89c3bf18dfd7
kingslayer281 <66055973+kingslayer281@users.noreply.github.com>
2021-07-17 14:50:45 +0100

    Price chart

---
+++ Tim/Price chart/Price_chart_script.js
@@ -0,0 +1,328 @@
+// BTC //
+const btcData = async () => {
+  const response = await fetch('https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=USD&limit=119&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146');
+  const json = await response.json();
+  const data = json.Data.Data
+  const times = data.map(obj => obj.time)
+  const prices = data.map(obj => obj.high)
+  return {
+    times,
+    prices
+  }
+}
+
+// BSV //
+const bsvData = async () => {
+  const response = await fetch('https://min-api.cryptocompare.com/data/v2/histominute?fsym=BSV&tsym=USD&limit=119&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146');
+  const json = await response.json();
+  const data = json.Data.Data
+  const times = data.map(obj => obj.time)
+  const prices = data.map(obj => obj.high)
+  return {
+    times,
+    prices
+  }
+}
+
+// ETH //
+const ethereumData = async () => {
+  const response = await fetch('https://min-api.cryptocompare.com/data/v2/histominute?fsym=ETH&tsym=USD&limit=119&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146');
+  const json = await response.json();
+  const data = json.Data.Data
+  const times = data.map(obj => obj.time)
+  const prices = data.map(obj => obj.high)
+  return {
+    times,
+    prices
+  }
+}
+
+
+// Error handling //
+function checkStatus(response) {
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+
+
+// Charts //
+let createBtcChart
+let createBsvChart
+let createethereumChart
+
+// BTC Funtion //
+async function printBtcChart() {
+  let { times, prices } = await btcData()
+
+  let btcChart = document.getElementById('btcChart').getContext('2d');
+
+  let gradient = btcChart.createLinearGradient(0, 0, 0, 400);
+
+  gradient.addColorStop(0, 'rgba(247,147,26,.5)');
+  gradient.addColorStop(.425, 'rgba(255,193,119,0)');
+
+  Chart.defaults.global.defaultFontFamily = 'Red Hat Text';
+  Chart.defaults.global.defaultFontSize = 12;
+
+  createBtcChart = new Chart(btcChart, {
+    type: 'line',
+    data: {
+      labels: times,
+      datasets: [{
+        label: '$',
+        data: prices,
+        backgroundColor: gradient,
+        borderColor: 'rgba(242, 169, 0)',
+        borderJoinStyle: 'round',
+        borderCapStyle: 'round',
+        borderWidth: 3,
+        pointRadius: 0,
+        pointHitRadius: 10,
+        lineTension: .2,
+      }]
+    },
+
+    options: {
+      title: {
+        display: false,
+        text: 'Bitcoin Chart!',
+        fontSize: 35
+      },
+
+      legend: {
+        display: false
+      },
+
+      layout: {
+        padding: {
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0
+        }
+      },
+
+      scales: {
+        xAxes: [{
+          display: false,
+          gridLines: {}
+        }],
+        yAxes: [{
+          display: false,
+          gridLines: {}
+        }]
+      },
+
+      tooltips: {
+        callbacks: {
+          
+          title: function() {}
+       },
+        displayColors: false,
+        yPadding: 10,
+        xPadding: 10,
+        position: 'nearest',
+        caretSize: 10,
+        backgroundColor: 'rgba(255,255,255,.9)',
+        bodyFontSize: 15,
+        bodyFontColor: '#303030' 
+      }
+    }
+  });
+}
+
+
+//BSV Funtion//
+async function printBsvChart() {
+  let { times, prices } = await bsvData()
+
+  let bsvChart = document.getElementById('bsvChart').getContext('2d');
+
+  let gradient = bsvChart.createLinearGradient(0, 0, 0, 400);
+
+  gradient.addColorStop(0, 'rgba(255, 255, 224)');
+  gradient.addColorStop(.425, 'rgba(255, 255, 196)');
+
+  Chart.defaults.global.defaultFontFamily = 'Red Hat Text';
+  Chart.defaults.global.defaultFontSize = 12;
+
+  createBsvChart = new Chart(bsvChart, {
+    type: 'line',
+    data: {
+      labels: times,
+      datasets: [{
+        label: "",
+        data: prices,
+        backgroundColor: gradient,
+        borderColor: 'rgba(255, 201, 86)',
+        borderJoinStyle: 'round',
+        borderCapStyle: 'round',
+        borderWidth: 3,
+        pointRadius: 0,
+        pointHitRadius: 10,
+        lineTension: .2,
+      }]
+    },
+
+    options: {
+      title: {
+        display: false,
+        text: 'Bitcoin SV Chart!',
+        fontSize: 35
+      },
+
+      legend: {
+        display: false
+      },
+
+      layout: {
+        padding: {
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0
+        }
+      },
+
+      scales: {
+        xAxes: [{
+          display: false,
+          gridLines: {}
+        }],
+        yAxes: [{
+          display: false,
+          gridLines: {}
+        }]
+      },
+
+      tooltips: {
+        callbacks: {
+          title: function() {}
+       },
+        displayColors: false,
+        yPadding: 10,
+        xPadding: 10,
+        position: 'nearest',
+        caretSize: 10,
+        backgroundColor: 'rgba(255,255,255,.9)',
+        bodyFontSize: 15,
+        bodyFontColor: '#303030' 
+      }
+    }
+  });
+}
+
+//ETH Funtion//
+async function printEthereumChart() {
+  let { times, prices } = await ethereumData()
+
+  let ethereumChart = document.getElementById('ethereumChart').getContext('2d');
+
+  let gradient = ethereumChart.createLinearGradient(0, 0, 0, 400);
+
+  gradient.addColorStop(0, 'rgba(78,56,216,.5)');
+  gradient.addColorStop(.425, 'rgba(118,106,192,0)');
+
+  Chart.defaults.global.defaultFontFamily = 'Red Hat Text';
+  Chart.defaults.global.defaultFontSize = 12;
+
+  createEthereumChart = new Chart(ethereumChart, {
+    type: 'line',
+    data: {
+      labels: times,
+      datasets: [{
+        label: '$',
+        data: prices,
+        backgroundColor: gradient,
+        borderColor: 'rgba(113, 107, 148)',
+        borderJoinStyle: 'round',
+        borderCapStyle: 'round',
+        borderWidth: 3,
+        pointRadius: 0,
+        pointHitRadius: 10,
+        lineTension: .2,
+      }]
+    },
+
+    options: {
+      title: {
+        display: false,
+        text: 'Ethereum Chart!',
+        fontSize: 35
+      },
+
+      legend: {
+        display: false
+      },
+
+      layout: {
+        padding: {
+          left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0
+        }
+      },
+
+      scales: {
+        xAxes: [{
+          display: false,
+          gridLines: {}
+        }],
+        yAxes: [{
+          display: false,
+          gridLines: {}
+        }]
+      },
+
+      tooltips: {
+        callbacks: {
+          title: function() {}
+       },
+        displayColors: false,
+        yPadding: 10,
+        xPadding: 10,
+        position: 'nearest',
+        caretSize: 10,
+        backgroundColor: 'rgba(255,255,255,.9)',
+        bodyFontSize: 15,
+        bodyFontColor: '#303030' 
+      }
+    }
+  });
+}
+
+
+// Update price funtion//
+async function updateEthereumPrice() {
+  let { times, prices } = await ethereumData()
+  let currentPrice = prices[prices.length-1].toFixed(2);
+
+  document.getElementById("ethPrice").innerHTML = "$" + currentPrice;
+}
+
+async function updateBsvPrice() {
+  let { times, prices } = await bsvData()
+  let currentPrice = prices[prices.length-1].toFixed(2);
+
+  document.getElementById("bsvPrice").innerHTML = "$" + currentPrice;
+}
+
+async function updateBitcoinPrice() {
+  let { times, prices } = await btcData()
+  let currentPrice = prices[prices.length-1].toFixed(2);
+
+  document.getElementById("btcPrice").innerHTML = "$" + currentPrice;
+}
+
+updateEthereumPrice()
+updateBsvPrice()
+updateBitcoinPrice()
+
+printBtcChart()
+printBsvChart()
+printEthereumChart()
